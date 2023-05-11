d5ebabc0dc07366c28e2b5a44d53c8583223af9a
David Arcibar <davidaarcibar@gmail.com>
2023-05-11 11:00:18 -0500

    update

---
+++ hand.js
@@ -0,0 +1,23 @@
+class Hand{
+    constructor(){
+        this.cards = []
+    }
+
+    addCard(card){
+        this.cards.push(card);
+    }
+
+    playCard(card){
+
+    }
+
+
+//     playCard(card) {
+//         const index = this.cards.indexOf(card);
+//         if (index !== -1) {
+//           return this.cards.splice(index, 1)[0];
+//         } else {
+//           return null;
+//         }
+//       }
+}
